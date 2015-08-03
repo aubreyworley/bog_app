07691033ce9ff42644d61778651a49376d8bd31a
aubreyworley <aubreyworley@gmail.com>
2015-08-02 22:14:19 -0700

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-Creature.create({name: "Luke", description: "Jedi"})
- Creature.create({name: "Darth Vader", description: "Father of Luke"})
+# Creature.create({name: "Luke", description: "Jedi"})
+#  Creature.create({name: "Darth Vader", description: "Father of Luke"})
